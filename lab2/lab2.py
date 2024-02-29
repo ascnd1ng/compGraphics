f25aaa3c5c7b5aa9932cc0549c3062f5703a04b3
ncoji <ncojin@yandex.ru>
2024-02-29 12:16:14 +0300

    i dont know

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -1,6 +1,6 @@
 import glfw
 from OpenGL.GL import *
-delta = 0.1
+delta = 0.01
 angle = 0.0
 
 def display(window):
@@ -39,29 +39,32 @@ def display(window):
         (1, 1, -1),
         (0, 1, -1)
     ]
+    vertices = [(x / 10, y / 10, z / 10) for x, y, z in vertices]
     colors = [(0, 0, 1), (0, 1, 0), (1, 0, 0)]
     global angle
     glClear(GL_COLOR_BUFFER_BIT)
     glLoadIdentity()
     glClearColor(1.0, 1.0, 1.0, 1.0)
     glPushMatrix()
-    glRotatef(angle, 0, 0, 1)  # вращение  (оси)
+    glRotatef(angle, 1, 1, 1)  # вращение  (оси)
     glBegin(GL_QUADS)
 
-
+    c = 0
     for i in range (0, len(vertices), 4):
+        c += 1
         index1, index2, index3, index4 = (i, i + 1, i + 2, i + 3)
         vertex1 = vertices[index1]
         vertex2 = vertices[index2]
         vertex3 = vertices[index3]
         vertex4 = vertices[index4]
 
-        glColor3f(*colors[(i // 4)% 3])
+        glColor3f(*colors[(i // 8)])
         glVertex3f(*vertex1)
         glVertex3f(*vertex2)
         glVertex3f(*vertex3)
         glVertex3f(*vertex4)
 
+    print(c)
     glEnd()
     glPopMatrix()
     glfw.swap_buffers(window)
@@ -76,9 +79,9 @@ mods):
     global angle
     if action == glfw.PRESS:
         if key == glfw.KEY_RIGHT:
-            delta = -0.1
+            delta = -0.01
         if key == glfw.KEY_LEFT:
-            delta = 0.1
+            delta = 0.01
         if key == glfw.KEY_SPACE:
             delta = 0
 
